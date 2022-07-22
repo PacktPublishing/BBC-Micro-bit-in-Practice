0214bd91c09ea4f4644c92ec5b416f6ab6fd3710
ASHWIN PAJANKAR <ashwin.pajankar@github.com>
2022-07-22 09:55:15 +0530

    Corrected the program

--- Chapter05/prog10.py
+++ Chapter05/prog10.py
@@ -4,38 +4,12 @@ if not display.is_on():
 else:
     print("Display is already on...")
 display.clear()
-pattern0 = Image("00000:"
-                 "11111:"
-                 "22222:"
-                 "33333:"
-                 "44444")
-pattern1 = Image("11111:"
-                 "22222:"
-                 "33333:"
-                 "44444:"
-                 "55555")
-pattern2 = Image("22222:"
-                 "33333:"
-                 "44444:"
-                 "55555:"
-                 "66666")
-pattern3 = Image("33333:"
-                 "44444:"
-                 "55555:"
-                 "66666:"
-                 "77777")
-pattern4 = Image("44444:"
-                 "55555:"
-                 "66666:"
-                 "77777:"
-                 "88888")
-pattern5 = Image("55555:"
-                 "66666:"
-                 "77777:"
-                 "88888:"
-                 "99999")
-patterns = [pattern0, pattern1,
-            pattern2, pattern2,
-            pattern3, pattern4,
-            pattern5]
+pattern0 = Image("00000:11111:22222:33333:44444")
+pattern1 = Image("11111:22222:33333:44444:55555")
+pattern2 = Image("22222:33333:44444:55555:66666")
+pattern3 = Image("33333:44444:55555:66666:77777")
+pattern4 = Image("44444:55555:66666:77777:88888")
+pattern5 = Image("55555:66666:77777:88888:99999")
+patterns = [pattern0, pattern1, pattern2,
+            pattern3, pattern4, pattern5]
 display.show(patterns, delay=100, loop=True)
