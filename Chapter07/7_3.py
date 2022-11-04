7dbbd5f11aa2073e07489336d75e33580274ff93
ASHWIN PAJANKAR <ashwin.pajankar@github.com>
2022-11-04 15:55:18 +0530

    updated filenames

--- Chapter07/7_3.py
+++ Chapter07/7_3.py
@@ -1,10 +1,10 @@
 from microbit import *
 
 while True:
-    if pin1.read_digital():
+    if pin2.read_digital():
         pin0.write_digital(1)
-        pin2.write_digital(0)
+        pin1.write_digital(0)
         sleep(1000)
     else:
-        pin2.write_digital(1)
+        pin1.write_digital(1)
         pin0.write_digital(0)
